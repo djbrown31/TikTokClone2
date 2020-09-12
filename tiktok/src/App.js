f3c16f1c289ebce2f1eb6917062fcd38ff1ab056
Dan Brown <brown.danny.j@gmail.com>
2020-09-12 14:36:34 -0500

    commit

---
+++ tiktok/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Video from './Video';
+
+function App() {
+  return (
+    <div className="app">
+
+      <div className="app__videos">
+        <Video />
+        <Video />
+      </div>
+
+      {/* app container */}
+        {/* videos */}
+        {/* <Video /> */}
+        {/* <Video /> */}
+        {/* <Video /> */}
+        {/* <Video /> */}
+
+    </div>
+  );
+}
+
+export default App;
